95c2d85d8f72ca39c922b547e2e93a64d05d3c24
JonnyKong <jonnykong1996@gmail.com>
2019-06-28 16:19:35 -0700

    Proto: Add RibStatusMessage. Recursively decode

--- ndncc/asyncndn.py
+++ ndncc/asyncndn.py
@@ -34,20 +34,31 @@ async def fetch_data_packet(face: Face, interest: Interest) -> Union[Data, Netwo
 
 
 def decode_dict(msg) -> dict:
+    """
+    Recursive decode ProtoBuf-type objects
+    TODO: Xinyu, is this recursion correct?
+    """
     ret = {}
     for field in msg.DESCRIPTOR.fields:
-        if field.type == field.TYPE_MESSAGE:
-            pass
-        elif (field.type == field.TYPE_UINT32 or
-              field.type == field.TYPE_UINT64):
+        if field.label == field.LABEL_REPEATED:
+            ret[field.name] = decode_list(getattr(msg, field.name))
+        elif field.type == field.TYPE_MESSAGE:
+            ret[field.name] = decode_dict(getattr(msg, field.name))
+        elif (field.type == field.TYPE_UINT32 or field.type == field.TYPE_UINT64):
             ret[field.name] = str(getattr(msg, field.name))
         elif field.type == field.TYPE_BYTES:
             ret[field.name] = getattr(msg, field.name).decode('utf-8')
     return ret
 
-
 def decode_list(lst) -> list:
+    """
+    Recursively decode ProtoBuf containers. Because Protobuf Scalar Containers will 
+    decode to python primitive types, need to process separately
+    """
     ret = []
     for item in lst:
-        ret.append(decode_dict(item))
+        if isinstance(item, (int, float, str, bool, bytes)):
+            ret.append(item)
+        else:
+            ret.append(decode_dict(item))
     return ret
--- ndncc/nfd_face_mgmt_pb2.py
+++ ndncc/nfd_face_mgmt_pb2.py
@@ -20,7 +20,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='ndn_message',
   syntax='proto2',
   serialized_options=None,
-  serialized_pb=_b('\n\x13nfd-face-mgmt.proto\x12\x0bndn_message\"\x19\n\x04Name\x12\x11\n\tcomponent\x18\x08 \x03(\x0c\"\xba\x02\n\x1c\x46\x61\x63\x65\x45ventNotificationMessage\x12\x61\n\x17\x66\x61\x63\x65_event_notification\x18\xc0\x01 \x02(\x0b\x32?.ndn_message.FaceEventNotificationMessage.FaceEventNotification\x1a\xb6\x01\n\x15\x46\x61\x63\x65\x45ventNotification\x12\x18\n\x0f\x66\x61\x63\x65_event_kind\x18\xc1\x01 \x02(\r\x12\x0f\n\x07\x66\x61\x63\x65_id\x18i \x02(\x04\x12\x0b\n\x03uri\x18r \x02(\x0c\x12\x12\n\tlocal_uri\x18\x81\x01 \x02(\x0c\x12\x13\n\nface_scope\x18\x84\x01 \x02(\r\x12\x19\n\x10\x66\x61\x63\x65_persistency\x18\x85\x01 \x02(\r\x12\x12\n\tlink_type\x18\x86\x01 \x02(\r\x12\r\n\x05\x66lags\x18l \x02(\x04\"\xa9\x03\n\x15\x43ontrolCommandMessage\x12P\n\x12\x63ontrol_parameters\x18h \x02(\x0b\x32\x34.ndn_message.ControlCommandMessage.ControlParameters\x1a\xbd\x02\n\x11\x43ontrolParameters\x12\x1f\n\x04name\x18\x07 \x01(\x0b\x32\x11.ndn_message.Name\x12\x0f\n\x07\x66\x61\x63\x65_id\x18i \x01(\x04\x12\x0b\n\x03uri\x18r \x01(\x0c\x12\x12\n\tlocal_uri\x18\x81\x01 \x01(\x0c\x12\x0e\n\x06origin\x18o \x01(\x04\x12\x0c\n\x04\x63ost\x18j \x01(\x04\x12\x11\n\x08\x63\x61pacity\x18\x83\x01 \x01(\x04\x12\x0e\n\x05\x63ount\x18\x84\x01 \x01(\x04\x12\x17\n\x0e\x62\x61se_cong_mark\x18\x87\x01 \x01(\x04\x12\x17\n\x0e\x64\x65\x66_cong_thres\x18\x88\x01 \x01(\x04\x12\x0c\n\x03mtu\x18\x89\x01 \x01(\x04\x12\r\n\x05\x66lags\x18l \x01(\x04\x12\x0c\n\x04mask\x18p \x01(\x04\x12#\n\x08strategy\x18k \x01(\x0b\x32\x11.ndn_message.Name\x12\x12\n\nexp_period\x18m \x01(\x04\"\x9c\x01\n\x16\x43ontrolResponseMessage\x12M\n\x10\x63ontrol_response\x18\x65 \x02(\x0b\x32\x33.ndn_message.ControlResponseMessage.ControlResponse\x1a\x33\n\x0f\x43ontrolResponse\x12\x0f\n\x07st_code\x18\x66 \x01(\x04\x12\x0f\n\x07st_text\x18g \x01(\x0c\"\xa4\x05\n\rGeneralStatus\x12\x14\n\x0bnfd_version\x18\x80\x01 \x01(\x0c\x12\x18\n\x0fstart_timestamp\x18\x81\x01 \x01(\x04\x12\x1a\n\x11\x63urrent_timestamp\x18\x82\x01 \x01(\x04\x12\x1c\n\x13n_name_tree_entries\x18\x83\x01 \x01(\x04\x12\x16\n\rn_fib_entries\x18\x84\x01 \x01(\x04\x12\x16\n\rn_pit_entries\x18\x85\x01 \x01(\x04\x12\x1e\n\x15n_measurement_entries\x18\x86\x01 \x01(\x04\x12\x15\n\x0cn_cs_entries\x18\x87\x01 \x01(\x04\x12\x17\n\x0en_in_interests\x18\x90\x01 \x01(\x04\x12\x12\n\tn_in_data\x18\x91\x01 \x01(\x04\x12\x13\n\nn_in_nacks\x18\x97\x01 \x01(\x04\x12\x18\n\x0fn_out_interests\x18\x92\x01 \x01(\x04\x12\x13\n\nn_out_data\x18\x93\x01 \x01(\x04\x12\x14\n\x0bn_out_nacks\x18\x98\x01 \x01(\x04\x12\x1e\n\x15n_satisfied_interests\x18\x99\x01 \x01(\x04\x12 \n\x17n_unsatisfied_interests\x18\x9a\x01 \x01(\x04\x12\x1f\n\x16n_fragmentation_errors\x18\xc8\x01 \x01(\x04\x12\x17\n\x0en_out_over_mtu\x18\xc9\x01 \x01(\x04\x12\x18\n\x0fn_in_lp_invalid\x18\xca\x01 \x01(\x04\x12\x1e\n\x15n_reassembly_timeouts\x18\xcb\x01 \x01(\x04\x12\x19\n\x10n_in_net_invalid\x18\xcc\x01 \x01(\x04\x12\x17\n\x0en_acknowledged\x18\xcd\x01 \x01(\x04\x12\x18\n\x0fn_retransmitted\x18\xce\x01 \x01(\x04\x12\x19\n\x10n_retx_exhausted\x18\xcf\x01 \x01(\x04\x12\x1c\n\x13n_congestion_marked\x18\xd0\x01 \x01(\x04\"\xee\x03\n\x11\x46\x61\x63\x65StatusMessage\x12?\n\x0b\x66\x61\x63\x65_status\x18\x80\x01 \x03(\x0b\x32).ndn_message.FaceStatusMessage.FaceStatus\x1a\x97\x03\n\nFaceStatus\x12\x0f\n\x07\x66\x61\x63\x65_id\x18i \x01(\x04\x12\x0b\n\x03uri\x18r \x01(\x0c\x12\x12\n\tlocal_uri\x18\x81\x01 \x01(\x0c\x12\x12\n\nexp_period\x18m \x01(\x04\x12\x13\n\nface_scope\x18\x84\x01 \x01(\x04\x12\x19\n\x10\x66\x61\x63\x65_persistency\x18\x85\x01 \x01(\x04\x12\x12\n\tlink_type\x18\x86\x01 \x01(\x04\x12\x17\n\x0e\x62\x61se_cong_mark\x18\x87\x01 \x01(\x04\x12\x17\n\x0e\x64\x65\x66_cong_thres\x18\x88\x01 \x01(\x04\x12\x0c\n\x03mtu\x18\x89\x01 \x01(\x04\x12\x17\n\x0en_in_interests\x18\x90\x01 \x01(\x04\x12\x12\n\tn_in_data\x18\x91\x01 \x01(\x04\x12\x13\n\nn_in_nacks\x18\x97\x01 \x01(\x04\x12\x18\n\x0fn_out_interests\x18\x92\x01 \x01(\x04\x12\x13\n\nn_out_data\x18\x93\x01 \x01(\x04\x12\x14\n\x0bn_out_nacks\x18\x98\x01 \x01(\x04\x12\x13\n\nn_in_bytes\x18\x94\x01 \x01(\x04\x12\x14\n\x0bn_out_bytes\x18\x95\x01 \x01(\x04\x12\r\n\x05\x66lags\x18l \x01(\x04')
+  serialized_pb=_b('\n\x13nfd-face-mgmt.proto\x12\x0bndn_message\"\x19\n\x04Name\x12\x11\n\tcomponent\x18\x08 \x03(\x0c\"\xba\x02\n\x1c\x46\x61\x63\x65\x45ventNotificationMessage\x12\x61\n\x17\x66\x61\x63\x65_event_notification\x18\xc0\x01 \x02(\x0b\x32?.ndn_message.FaceEventNotificationMessage.FaceEventNotification\x1a\xb6\x01\n\x15\x46\x61\x63\x65\x45ventNotification\x12\x18\n\x0f\x66\x61\x63\x65_event_kind\x18\xc1\x01 \x02(\r\x12\x0f\n\x07\x66\x61\x63\x65_id\x18i \x02(\x04\x12\x0b\n\x03uri\x18r \x02(\x0c\x12\x12\n\tlocal_uri\x18\x81\x01 \x02(\x0c\x12\x13\n\nface_scope\x18\x84\x01 \x02(\r\x12\x19\n\x10\x66\x61\x63\x65_persistency\x18\x85\x01 \x02(\r\x12\x12\n\tlink_type\x18\x86\x01 \x02(\r\x12\r\n\x05\x66lags\x18l \x02(\x04\"\xa9\x03\n\x15\x43ontrolCommandMessage\x12P\n\x12\x63ontrol_parameters\x18h \x02(\x0b\x32\x34.ndn_message.ControlCommandMessage.ControlParameters\x1a\xbd\x02\n\x11\x43ontrolParameters\x12\x1f\n\x04name\x18\x07 \x01(\x0b\x32\x11.ndn_message.Name\x12\x0f\n\x07\x66\x61\x63\x65_id\x18i \x01(\x04\x12\x0b\n\x03uri\x18r \x01(\x0c\x12\x12\n\tlocal_uri\x18\x81\x01 \x01(\x0c\x12\x0e\n\x06origin\x18o \x01(\x04\x12\x0c\n\x04\x63ost\x18j \x01(\x04\x12\x11\n\x08\x63\x61pacity\x18\x83\x01 \x01(\x04\x12\x0e\n\x05\x63ount\x18\x84\x01 \x01(\x04\x12\x17\n\x0e\x62\x61se_cong_mark\x18\x87\x01 \x01(\x04\x12\x17\n\x0e\x64\x65\x66_cong_thres\x18\x88\x01 \x01(\x04\x12\x0c\n\x03mtu\x18\x89\x01 \x01(\x04\x12\r\n\x05\x66lags\x18l \x01(\x04\x12\x0c\n\x04mask\x18p \x01(\x04\x12#\n\x08strategy\x18k \x01(\x0b\x32\x11.ndn_message.Name\x12\x12\n\nexp_period\x18m \x01(\x04\"\x9c\x01\n\x16\x43ontrolResponseMessage\x12M\n\x10\x63ontrol_response\x18\x65 \x02(\x0b\x32\x33.ndn_message.ControlResponseMessage.ControlResponse\x1a\x33\n\x0f\x43ontrolResponse\x12\x0f\n\x07st_code\x18\x66 \x01(\x04\x12\x0f\n\x07st_text\x18g \x01(\x0c\"\xa4\x05\n\rGeneralStatus\x12\x14\n\x0bnfd_version\x18\x80\x01 \x01(\x0c\x12\x18\n\x0fstart_timestamp\x18\x81\x01 \x01(\x04\x12\x1a\n\x11\x63urrent_timestamp\x18\x82\x01 \x01(\x04\x12\x1c\n\x13n_name_tree_entries\x18\x83\x01 \x01(\x04\x12\x16\n\rn_fib_entries\x18\x84\x01 \x01(\x04\x12\x16\n\rn_pit_entries\x18\x85\x01 \x01(\x04\x12\x1e\n\x15n_measurement_entries\x18\x86\x01 \x01(\x04\x12\x15\n\x0cn_cs_entries\x18\x87\x01 \x01(\x04\x12\x17\n\x0en_in_interests\x18\x90\x01 \x01(\x04\x12\x12\n\tn_in_data\x18\x91\x01 \x01(\x04\x12\x13\n\nn_in_nacks\x18\x97\x01 \x01(\x04\x12\x18\n\x0fn_out_interests\x18\x92\x01 \x01(\x04\x12\x13\n\nn_out_data\x18\x93\x01 \x01(\x04\x12\x14\n\x0bn_out_nacks\x18\x98\x01 \x01(\x04\x12\x1e\n\x15n_satisfied_interests\x18\x99\x01 \x01(\x04\x12 \n\x17n_unsatisfied_interests\x18\x9a\x01 \x01(\x04\x12\x1f\n\x16n_fragmentation_errors\x18\xc8\x01 \x01(\x04\x12\x17\n\x0en_out_over_mtu\x18\xc9\x01 \x01(\x04\x12\x18\n\x0fn_in_lp_invalid\x18\xca\x01 \x01(\x04\x12\x1e\n\x15n_reassembly_timeouts\x18\xcb\x01 \x01(\x04\x12\x19\n\x10n_in_net_invalid\x18\xcc\x01 \x01(\x04\x12\x17\n\x0en_acknowledged\x18\xcd\x01 \x01(\x04\x12\x18\n\x0fn_retransmitted\x18\xce\x01 \x01(\x04\x12\x19\n\x10n_retx_exhausted\x18\xcf\x01 \x01(\x04\x12\x1c\n\x13n_congestion_marked\x18\xd0\x01 \x01(\x04\"\xee\x03\n\x11\x46\x61\x63\x65StatusMessage\x12?\n\x0b\x66\x61\x63\x65_status\x18\x80\x01 \x03(\x0b\x32).ndn_message.FaceStatusMessage.FaceStatus\x1a\x97\x03\n\nFaceStatus\x12\x0f\n\x07\x66\x61\x63\x65_id\x18i \x01(\x04\x12\x0b\n\x03uri\x18r \x01(\x0c\x12\x12\n\tlocal_uri\x18\x81\x01 \x01(\x0c\x12\x12\n\nexp_period\x18m \x01(\x04\x12\x13\n\nface_scope\x18\x84\x01 \x01(\x04\x12\x19\n\x10\x66\x61\x63\x65_persistency\x18\x85\x01 \x01(\x04\x12\x12\n\tlink_type\x18\x86\x01 \x01(\x04\x12\x17\n\x0e\x62\x61se_cong_mark\x18\x87\x01 \x01(\x04\x12\x17\n\x0e\x64\x65\x66_cong_thres\x18\x88\x01 \x01(\x04\x12\x0c\n\x03mtu\x18\x89\x01 \x01(\x04\x12\x17\n\x0en_in_interests\x18\x90\x01 \x01(\x04\x12\x12\n\tn_in_data\x18\x91\x01 \x01(\x04\x12\x13\n\nn_in_nacks\x18\x97\x01 \x01(\x04\x12\x18\n\x0fn_out_interests\x18\x92\x01 \x01(\x04\x12\x13\n\nn_out_data\x18\x93\x01 \x01(\x04\x12\x14\n\x0bn_out_nacks\x18\x98\x01 \x01(\x04\x12\x13\n\nn_in_bytes\x18\x94\x01 \x01(\x04\x12\x14\n\x0bn_out_bytes\x18\x95\x01 \x01(\x04\x12\r\n\x05\x66lags\x18l \x01(\x04\"\x95\x02\n\x10RibStatusMessage\x12:\n\trib_entry\x18\x80\x01 \x03(\x0b\x32&.ndn_message.RibStatusMessage.RibEntry\x1a\xc4\x01\n\x08RibEntry\x12\x1f\n\x04name\x18\x07 \x01(\x0b\x32\x11.ndn_message.Name\x12<\n\x05route\x18\x81\x01 \x03(\x0b\x32,.ndn_message.RibStatusMessage.RibEntry.Route\x1aY\n\x05Route\x12\x0f\n\x07\x66\x61\x63\x65_id\x18i \x01(\x04\x12\x0e\n\x06origin\x18o \x01(\x04\x12\x0c\n\x04\x63ost\x18j \x01(\x04\x12\r\n\x05\x66lags\x18l \x01(\x04\x12\x12\n\nexp_period\x18m \x01(\x04')
 )
 
 
@@ -779,6 +779,132 @@ _FACESTATUSMESSAGE = _descriptor.Descriptor(
   serialized_end=2141,
 )
 
+
+_RIBSTATUSMESSAGE_RIBENTRY_ROUTE = _descriptor.Descriptor(
+  name='Route',
+  full_name='ndn_message.RibStatusMessage.RibEntry.Route',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='face_id', full_name='ndn_message.RibStatusMessage.RibEntry.Route.face_id', index=0,
+      number=105, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='origin', full_name='ndn_message.RibStatusMessage.RibEntry.Route.origin', index=1,
+      number=111, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='cost', full_name='ndn_message.RibStatusMessage.RibEntry.Route.cost', index=2,
+      number=106, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='flags', full_name='ndn_message.RibStatusMessage.RibEntry.Route.flags', index=3,
+      number=108, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='exp_period', full_name='ndn_message.RibStatusMessage.RibEntry.Route.exp_period', index=4,
+      number=109, type=4, cpp_type=4, label=1,
+      has_default_value=False, default_value=0,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=2332,
+  serialized_end=2421,
+)
+
+_RIBSTATUSMESSAGE_RIBENTRY = _descriptor.Descriptor(
+  name='RibEntry',
+  full_name='ndn_message.RibStatusMessage.RibEntry',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='name', full_name='ndn_message.RibStatusMessage.RibEntry.name', index=0,
+      number=7, type=11, cpp_type=10, label=1,
+      has_default_value=False, default_value=None,
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+    _descriptor.FieldDescriptor(
+      name='route', full_name='ndn_message.RibStatusMessage.RibEntry.route', index=1,
+      number=129, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[_RIBSTATUSMESSAGE_RIBENTRY_ROUTE, ],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=2225,
+  serialized_end=2421,
+)
+
+_RIBSTATUSMESSAGE = _descriptor.Descriptor(
+  name='RibStatusMessage',
+  full_name='ndn_message.RibStatusMessage',
+  filename=None,
+  file=DESCRIPTOR,
+  containing_type=None,
+  fields=[
+    _descriptor.FieldDescriptor(
+      name='rib_entry', full_name='ndn_message.RibStatusMessage.rib_entry', index=0,
+      number=128, type=11, cpp_type=10, label=3,
+      has_default_value=False, default_value=[],
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      serialized_options=None, file=DESCRIPTOR),
+  ],
+  extensions=[
+  ],
+  nested_types=[_RIBSTATUSMESSAGE_RIBENTRY, ],
+  enum_types=[
+  ],
+  serialized_options=None,
+  is_extendable=False,
+  syntax='proto2',
+  extension_ranges=[],
+  oneofs=[
+  ],
+  serialized_start=2144,
+  serialized_end=2421,
+)
+
 _FACEEVENTNOTIFICATIONMESSAGE_FACEEVENTNOTIFICATION.containing_type = _FACEEVENTNOTIFICATIONMESSAGE
 _FACEEVENTNOTIFICATIONMESSAGE.fields_by_name['face_event_notification'].message_type = _FACEEVENTNOTIFICATIONMESSAGE_FACEEVENTNOTIFICATION
 _CONTROLCOMMANDMESSAGE_CONTROLPARAMETERS.fields_by_name['name'].message_type = _NAME
@@ -789,12 +915,18 @@ _CONTROLRESPONSEMESSAGE_CONTROLRESPONSE.containing_type = _CONTROLRESPONSEMESSAG
 _CONTROLRESPONSEMESSAGE.fields_by_name['control_response'].message_type = _CONTROLRESPONSEMESSAGE_CONTROLRESPONSE
 _FACESTATUSMESSAGE_FACESTATUS.containing_type = _FACESTATUSMESSAGE
 _FACESTATUSMESSAGE.fields_by_name['face_status'].message_type = _FACESTATUSMESSAGE_FACESTATUS
+_RIBSTATUSMESSAGE_RIBENTRY_ROUTE.containing_type = _RIBSTATUSMESSAGE_RIBENTRY
+_RIBSTATUSMESSAGE_RIBENTRY.fields_by_name['name'].message_type = _NAME
+_RIBSTATUSMESSAGE_RIBENTRY.fields_by_name['route'].message_type = _RIBSTATUSMESSAGE_RIBENTRY_ROUTE
+_RIBSTATUSMESSAGE_RIBENTRY.containing_type = _RIBSTATUSMESSAGE
+_RIBSTATUSMESSAGE.fields_by_name['rib_entry'].message_type = _RIBSTATUSMESSAGE_RIBENTRY
 DESCRIPTOR.message_types_by_name['Name'] = _NAME
 DESCRIPTOR.message_types_by_name['FaceEventNotificationMessage'] = _FACEEVENTNOTIFICATIONMESSAGE
 DESCRIPTOR.message_types_by_name['ControlCommandMessage'] = _CONTROLCOMMANDMESSAGE
 DESCRIPTOR.message_types_by_name['ControlResponseMessage'] = _CONTROLRESPONSEMESSAGE
 DESCRIPTOR.message_types_by_name['GeneralStatus'] = _GENERALSTATUS
 DESCRIPTOR.message_types_by_name['FaceStatusMessage'] = _FACESTATUSMESSAGE
+DESCRIPTOR.message_types_by_name['RibStatusMessage'] = _RIBSTATUSMESSAGE
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
 Name = _reflection.GeneratedProtocolMessageType('Name', (_message.Message,), dict(
@@ -870,6 +1002,29 @@ FaceStatusMessage = _reflection.GeneratedProtocolMessageType('FaceStatusMessage'
   ))
 _sym_db.RegisterMessage(FaceStatusMessage)
 _sym_db.RegisterMessage(FaceStatusMessage.FaceStatus)
+
+RibStatusMessage = _reflection.GeneratedProtocolMessageType('RibStatusMessage', (_message.Message,), dict(
+
+  RibEntry = _reflection.GeneratedProtocolMessageType('RibEntry', (_message.Message,), dict(
+
+    Route = _reflection.GeneratedProtocolMessageType('Route', (_message.Message,), dict(
+      DESCRIPTOR = _RIBSTATUSMESSAGE_RIBENTRY_ROUTE,
+      __module__ = 'nfd_face_mgmt_pb2'
+      # @@protoc_insertion_point(class_scope:ndn_message.RibStatusMessage.RibEntry.Route)
+      ))
+    ,
+    DESCRIPTOR = _RIBSTATUSMESSAGE_RIBENTRY,
+    __module__ = 'nfd_face_mgmt_pb2'
+    # @@protoc_insertion_point(class_scope:ndn_message.RibStatusMessage.RibEntry)
+    ))
+  ,
+  DESCRIPTOR = _RIBSTATUSMESSAGE,
+  __module__ = 'nfd_face_mgmt_pb2'
+  # @@protoc_insertion_point(class_scope:ndn_message.RibStatusMessage)
+  ))
+_sym_db.RegisterMessage(RibStatusMessage)
+_sym_db.RegisterMessage(RibStatusMessage.RibEntry)
+_sym_db.RegisterMessage(RibStatusMessage.RibEntry.Route)
 
 
 # @@protoc_insertion_point(module_scope)
