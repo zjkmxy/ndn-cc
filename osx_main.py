bc07999e2ba7e7fe697ca76fe916674570b01198
Xinyu Ma <bitmxy@gmail.com>
2019-07-11 00:37:26 -0700

    NFD control

--- osx_main.py
+++ osx_main.py
@@ -10,6 +10,10 @@ if __name__ == '__main__':
     if child_pid == 0:
         app_main()
     else:
+        def sudo_execute(cmd):
+            script_text = 'do shell script "{}" with administrator privileges'.format(cmd)
+            os.spawnlp(os.P_NOWAIT, 'osascript', 'osascript', '-e', script_text)
+
         def setup(ico):
             ico.visible = True
 
@@ -19,8 +23,19 @@ if __name__ == '__main__':
 
         def show(_ico):
             os.spawnlp(os.P_NOWAIT, 'open', 'open', 'http://localhost:5000/')
+
+        def nfd_start(_ico):
+            sudo_execute('nfd-start')
+
+        def nfd_stop(_ico):
+            sudo_execute('nfd-stop')
+
         menu = pystray.Menu(
             pystray.MenuItem('Show', show),
+            pystray.Menu.SEPARATOR,
+            pystray.MenuItem('NFD Start', nfd_start),
+            pystray.MenuItem('NFD Stop', nfd_stop),
+            pystray.Menu.SEPARATOR,
             pystray.MenuItem('Exit', shutdown),
         )
 
